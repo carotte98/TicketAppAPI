4b846dc0624d2f057754561b092ada3673ec037a
Thierry Mordant <thierrymordant@ymail.com>
2026-05-20 12:19:50 +0200

    <feature> Ajout du Controlleur pour Ticket

---
+++ TicketApp/Controllers/TicketController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketApp.Models.DTOs;
+using TicketApp.Services.Interfaces;
+
+namespace TicketApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+        private readonly ILogger<TicketController> _logger;
+
+        public TicketController(ITicketService ticketService, ILogger<TicketController> logger)
+        {
+            _ticketService = ticketService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            _logger.LogInformation("Récupération des tickets");
+
+            var result = await _ticketService.GetAllAsync();
+
+            return Ok(result);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            _logger.LogInformation("Récupération du ticket à l'{id}", id);
+
+            var ticket = await _ticketService.GetByIdAsync(id);
+
+            if (ticket == null)
+                return NotFound();
+
+            return Ok(ticket);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTicketDto dto)
+        {
+            var ticket = await _ticketService.CreateAsync(dto);
+
+            return CreatedAtAction(nameof(GetById), new { id = ticket.idTicket }, ticket);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateTicketDto dto)
+        {
+            var ticket = await _ticketService.UpdateAsync(id, dto);
+
+            if (ticket == null)
+                return NotFound();
+
+            return Ok(ticket);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var success = await _ticketService.DeleteAsync(id);
+
+            if (!success)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
